0b7598c0632392522eaa71af3be1577b21df599f
Bruno Rodrigues Franco <bruno.rodrigues.franco1@gmail.com>
2023-09-15 17:51:08 +0100

    Moving base.html
    Moved base.html into the central folder for reuse by other modules

--- settings.py
+++ settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'pyshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
